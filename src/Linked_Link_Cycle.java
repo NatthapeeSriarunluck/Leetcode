7c5590e354b5697a20b95b9c4f8fbb56771aedcb
Natthapee Sriarunluck <natthapeesri@gmail.com>
2022-12-12 19:19:00 +0700

    initial commit

--- src/Linked_Link_Cycle.java
+++ src/Linked_Link_Cycle.java
@@ -3,6 +3,13 @@ import java.util.Set;
 
 public class Linked_Link_Cycle {
     public boolean hasCycle(ListNode head) {
-
+        if (head == null) return false;
+        ListNode slow = head; ListNode fast = head.next;
+        while (fast != null && fast.next != null) {
+            if (slow == fast) return true;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return false;
     }
 }
